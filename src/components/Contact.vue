e6b680f61863c3c09091b52411f749a47ef35328
shervan <shervangh@gmail.com>
2022-05-03 01:09:27 +0430

    create section for contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-zinc-900 w-full">
+    <div class="max-w-3xl mx-auto my-20 p-10">
+      <span class="bg-zinc-800 text-zinc-600 p-1">NEWS</span>
+      <h2 class="flex-1 text-white my-4 text-3xl">Lastest News</h2>
+      <input class="text-white bg-zinc-900 placeholder:text-white pl-2 w-full border my-3 h-10 border-zinc-600" placeholder="Name" type="text">
+      <input class="text-white bg-zinc-900 placeholder:text-white pl-2 w-full border my-3 h-10 border-zinc-600" placeholder="Email" type="email">
+      <textarea class="text-white bg-zinc-900 placeholder:text-white pl-2 w-full border my-3 h-20 border-zinc-600" placeholder="Message" type="text" />
+      <button class="bg-white transition-all duration-300 p-4 hover:pr-6 hover:tracking-widest">Send Message</button>
+    </div>
+  </div>
+</template>
+
