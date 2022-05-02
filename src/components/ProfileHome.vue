2ac842d8cb6bfe11bb042fd78fc8fd99ae0756f3
shervan <shervangh@gmail.com>
2022-05-03 01:16:50 +0430

    create profile

---
+++ src/components/ProfileHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-zinc-900 overflow-y-scroll w-full pb-20 overflow-hidden">
+    <div class="relative grid xl:gap-10 top-40 justify-center xl:top-44 pb-10 lg:grid xl:flex my-20  max-w-4xl mx-auto">
+      <img class="relative rounded-full left-2/4 xl:left-20 -translate-x-2/4 w-40 h-40 md:w-80 md:h-80 lg:w-80 lg:h-80 xl:w-80 xl:h-80 " src="../assets/images/2.5528a6dc.jpg" alt="photo">
+      <div class="text-center mt-20">
+        <h1 class="text-3xl md:text-5xl lg:text-5xl xl:text-5xl text-white font-bold">ADRIANO SMITH</h1>
+        <p class="text-zinc-400 px-2 my-5 italic">Creative Photographer based in New York and happy
+          <br>
+          to travel all over Europe to capture photos.
+        </p>
+        <div>
+        <i class="fa fa-facebook cursor-pointer text-white text-xl" aria-hidden="true"></i>
+        <i class="fa fa-instagram cursor-pointer text-white text-xl ml-4" aria-hidden="true"></i>
+        <i class="fa fa-dribbble cursor-pointer text-white text-xl ml-4" aria-hidden="true"></i>
+        <i class="fa fa-youtube-play cursor-pointer text-white text-xl ml-4" aria-hidden="true"></i>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
